255d3f60ceae4f10ff4b286c2faba3f93fb0a468
abiyan <abiyanfajar@gmail.com>
2026-02-15 22:41:34 +0700

    add package to github

--- backend/server.js
+++ backend/server.js
@@ -105,7 +105,7 @@ app.post('/api/seed', async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
